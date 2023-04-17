3920babf0770310a490dd999d933f26da55c308c
Bala <itabalaabdul31@yahoo.com>
2023-04-17 22:45:20 +0100

    Chat Model

---
+++ Plogg-API/Models/ChatModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Plogg_API.Models
+{
+    public class ChatModel
+    {
+        public Guid Id { get; set; }
+        public DateTime _userMessageTime { get; set; }
+        public String userMessage { get; set; }
+        public bool isSent { get; set; }
+        public bool isDelivered { get; set; }
+        public bool hasAttachment { get; set; }
+        public bool isReply { get; set; }
+        
+    }
+}
